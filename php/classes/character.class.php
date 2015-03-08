451bfb98bf6f84bc17fa9820aa229ab4f1756597
Molotovs <henrikn@live.se>
2015-03-08 20:53:21 +0100

    Old version

--- php/classes/character.class.php
+++ php/classes/character.class.php
@@ -1,6 +1,6 @@
 <?php
 	
-	class Character extends Challenges {
+	class Character extends Base {
  // Thief, Cleric, Warrior, Acrobat
 	public $name;
 	public $agility;
@@ -16,62 +16,4 @@
 
 	}
 
-//$this kallar man bara på i klasserna. 
-
-
-
-	public function has_won(){
-		if ($this->success>=100) {
-			echo($this->name." has won the game!".'<br>');
-		}
-		else {
-			echo($this->name." has not won the game.".'<br>');
-			}
-	}
-
-	public function acceptChallenge(){
-
-	}
-
-	public function changeChallenge(){
-
-	}
-
-	public function carryOutChallenge() {
-			
-	}
-	
-
-	public function carryOutChallengeWithCompanion(){
-
-	}
-
-	public function winTool(){
-
-	}
-
-	public function looseTool(){
-
-	}
-
-	public function getAgility(){
-  return $this->agility;
-	}
-
-	public function getSuccess(){
-		return $this->success;
-	}
-
-	public function getStrength(){
-		return $this->strength;
-	}
-
-	public function getLuck(){
-		return $this->luck;
-	}
-
-	public function getCunning(){
-		return $this->cunning;
-	}
-	
 }
